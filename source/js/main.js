46bfd7cfc67042b47753246d4a45e395a8660cc2
CainStr <77696629+CainStr@users.noreply.github.com>
2021-05-16 19:37:08 +0300

    Файл к предстоящей неделе

---
+++ source/js/main.js
@@ -0,0 +1,6 @@
+let navigationM = document.querySelector('.page__img-text::after');
+let themeButton = document.querySelector('.theme-button');
+
+themeButton.onclick = function() {
+  page.classList.toggle('light-theme');
+};
